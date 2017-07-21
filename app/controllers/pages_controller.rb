33f429d14d49c0e0a5399f3973b22114bd99a41f
Arthur Littmann <arthur.littmann@gmail.com>
2017-07-21 18:21:41 +0100

    balls are working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
   def home
+    @features = [
+        {
+          title: "Web developer",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam officiis nemo eos, ipsum culpa, necessitatibus expedita voluptas libero accusamus ipsa, earum aliquam velit. Amet, nobis, iusto? Ratione vero, harum voluptas!",
+          icon: "fa fa-code"
+        },
+        {
+          title: "Teacher",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam officiis nemo eos, ipsum culpa, necessitatibus expedita voluptas libero accusamus ipsa, earum aliquam velit. Amet, nobis, iusto? Ratione vero, harum voluptas!",
+          icon: "fa fa-graduation-cap"
+        },
+        {
+          title: "Business developer",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam officiis nemo eos, ipsum culpa, necessitatibus expedita voluptas libero accusamus ipsa, earum aliquam velit. Amet, nobis, iusto? Ratione vero, harum voluptas!",
+          icon: "fa fa-usd"
+        }
+      ]
   end
 end
