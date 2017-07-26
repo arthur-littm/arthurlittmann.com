d8e557ced7a5e63e56d00de1e166d02d33b1e22c
Arthur Littmann <arthur.littmann@gmail.com>
2017-07-26 22:42:05 +0100

    added slider for videos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,7 +23,7 @@ class PagesController < ApplicationController
     youtube_doc = Nokogiri::HTML(youtube)
     links = youtube_doc.css('div.compact-shelf-content-container .yt-uix-sessionlink').map { |link| link['href'] }
     @latest_videos_ids = []
-    links.uniq.first(3).each do |link|
+    links.uniq.first(9).each do |link|
       @latest_videos_ids << link.split("=")[-1]
     end
   end
